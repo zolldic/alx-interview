1f4778671eee8685a50f91de7f7390788733482e
sai <ojiip95@gmail.com>
2024-08-22 15:20:50 +0200

    refactor: checking every single bit

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,9 +5,27 @@
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encodin:1
     """
+    bytes_number = 0
     try:
         bytes(data)
+        for byte in data:
+            lsb = byte & 0b11111111
+
+            if bytes_number == 0:
+                if lsb < 0x80:
+                    bytes_number = 0
+                elif lsb < 0xC2:
+                    return False
+                elif lsb < 0xE0:
+                    bytes_number = 2
+                elif lsb < 0xF0:
+                    bytes_number = 3
+                else:
+                    return False
+            else:
+                if lsb < 0x80:
+                    return False
+                bytes_number -= 1
+        return bytes_number == 0
     except ValueError:
         return False
-
-    return True
