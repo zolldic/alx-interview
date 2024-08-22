1c6a7662199b77cd492f9cc8c33a54e9a20b0ea0
sai <ojiip95@gmail.com>
2024-08-22 15:35:06 +0200

    refactor: another approach

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,25 +6,28 @@ def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encodin:1
     """
     bytes_number = 0
-    for byte in data:
-        if byte < 0 or byte > 255:
-            return False
+    mask1 = 1 << 7
+    mask2 = 1 << 6
 
+    for byte in data:
         lsb = byte & 0b11111111
 
         if bytes_number == 0:
-            if lsb < 0x80:
-                bytes_number = 0
-            elif lsb < 0xC2:
+            if (lsb & mask1) == 0:
+                continue
+            elif (lsb & mask1) != 0 and (lsb & mask2) == 0:
                 return False
-            elif lsb < 0xE0:
-                bytes_number = 2
-            elif lsb < 0xF0:
-                bytes_number = 3
             else:
-                return False
+                bytes_number = 0
+                mask = mask1
+                while (byte & mask) != 0:
+                    bytes_number += 1
+                    mask >>= 1
+
+                if bytes_number == 1 or bytes_number > 4:
+                    return False
         else:
-            if lsb < 0x80:
+            if not (byte & mask1 != 0 and byte & mask2 == 0):
                 return False
-            bytes_number -= 1
+        bytes_number -= 1
     return bytes_number == 0
