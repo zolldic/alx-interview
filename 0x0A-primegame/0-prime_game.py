e6861ac6b70fbbc82c4f46df2ec99e1f7d5d0919
sai <ojiip95@gmail.com>
2024-10-17 23:18:03 +0200

    build: task 0

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""0. Prime Game """
+
+def isWinner(x, nums):
+    """Return: name of the player that won the most rounds"""
+    if x < 1 or not nums:
+        return None
+
+    max_n = max(nums)
+    sieve = [True] * (max_n + 1)
+    sieve[0] = sieve[1] = False
+
+    for i in range(2, int(max_n ** 0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, max_n + 1, i):
+                sieve[j] = False
+
+    prime_count = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if sieve[i] else 0)
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if prime_count[n] % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
+
